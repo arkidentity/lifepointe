fcbb3b740f1c8cfa17ce1853424d95ca69a67cfe
arkidentity <thearkidentity@gmail.com>
2026-05-09 17:35:33 -0500

    fix

--- components/visit/LifepointeKids.tsx
+++ components/visit/LifepointeKids.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
 import derekImg from '@/images/team/derek-van-dyke-kids.png'
--- components/visit/VisitHero.tsx
+++ components/visit/VisitHero.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 import buildingImg from '@/images/lpc-building.jpg'
 
